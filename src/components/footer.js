ed8bedb146095c07b51542a812df81294b095131
Manuel Lang <lang@reasonance.de>
2026-02-22 14:13:37 +0100

    Merge

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
@@ -11,6 +11,33 @@ const StyledFooter = styled.footer`
   min-height: 70px;
   padding: 15px;
   text-align: center;
+`;
+
+const StyledSocialLinks = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    width: 100%;
+    max-width: 270px;
+    margin: 0 auto 10px;
+    color: var(--light-slate);
+  }
+
+  ul {
+    ${({ theme }) => theme.mixins.flexBetween};
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    a {
+      padding: 10px;
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
 `;
 
 const StyledCredit = styled.div`
@@ -41,37 +68,61 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
+
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
+    fetch('https://api.github.com/repos/bchiang7/v4')
+      .then(response => response.json())
+      .then(json => {
+        const { stargazers_count, forks_count } = json;
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        });
+      })
+      .catch(e => console.error(e));
   }, []);
 
   return (
-    <>
-      <StyledLeftSideSocialLinks>
+    <StyledFooter>
+      <StyledSocialLinks>
         <ul>
           {socialMedia &&
             socialMedia.map(({ name, url }, i) => (
               <li key={i}>
-                <a href={url} aria-label={name} target="_blank" rel="noopener noreferrer">
+                <a href={url} aria-label={name}>
                   <Icon name={name} />
                 </a>
               </li>
             ))}
         </ul>
-      </StyledLeftSideSocialLinks>
-      <StyledFooter>
-        <StyledCredit tabIndex="-1">
-          <div>2024 Manuel Lang</div>
-          <div>
-            <a href="https://github.com/bchiang7/v4" target="_blank" rel="noopener noreferrer">
-              Check out the inspiration
-            </a>
-          </div>
-        </StyledCredit>
-      </StyledFooter>
-    </>
+      </StyledSocialLinks>
+
+      <StyledCredit tabindex="-1">
+        <a href="https://github.com/bchiang7/v4">
+          <div>Designed &amp; Built by Brittany Chiang</div>
+
+          {githubInfo.stars && githubInfo.forks && (
+            <div className="github-stats">
+              <span>
+                <Icon name="Star" />
+                <span>{githubInfo.stars.toLocaleString()}</span>
+              </span>
+              <span>
+                <Icon name="Fork" />
+                <span>{githubInfo.forks.toLocaleString()}</span>
+              </span>
+            </div>
+          )}
+        </a>
+      </StyledCredit>
+    </StyledFooter>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, Contact } from '@components';
+import { About, Featured, Layout, Hero, Contact, Projects } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -16,8 +16,9 @@ const IndexPage = ({ location }) => {
     <Layout location={location}>
       <StyledMainContainer className="fillHeight">
         <Hero />
-        {/* <Featured /> */}
-        {/* <Projects /> */}
+        <About />
+        <Featured />
+        <Projects />
         <Contact />
       </StyledMainContainer>
     </Layout>
