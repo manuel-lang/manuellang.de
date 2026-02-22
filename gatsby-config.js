7b10794a195dfd73e68d4fc6303504ab29f76d1f
Manuel Lang <lang@reasonance.de>
2026-02-22 12:21:10 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Manuel Lang',
+        short_name: 'Manuel Lang',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import { Layout, Hero, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
-const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
-);
+const IndexPage = ({ location }) => {
+  useEffect(() => {
+    // Scroll to top on component mount
+  }, []);
+
+  return (
+    <Layout location={location}>
+      <StyledMainContainer className="fillHeight">
+        <Hero />
+        {/* <Featured /> */}
+        {/* <Projects /> */}
+        <Contact />
+      </StyledMainContainer>
+    </Layout>
+  );
+};
 
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
